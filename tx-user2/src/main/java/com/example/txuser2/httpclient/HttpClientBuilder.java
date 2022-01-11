99e464c53a2202d18488e95f70d9347146bc2c66
yoxiyoxiiii <846814595@qq.com>
2022-01-11 03:13:39 +0000

    Delete tx-user2/src/main/java/com/example/txuser2 directory

--- tx-user2/src/main/java/com/example/txuser2/httpclient/HttpClientBuilder.java
+++
@@ -1,6 +0,0 @@
-package com.example.txuser2.httpclient;
-
-public interface HttpClientBuilder {
-
-     HttpClientUtil  builder();
-}
--- tx-user2/src/main/java/com/example/txuser2/service/UserBService.java
+++
@@ -1,8 +0,0 @@
-package com.example.txuser2.service;
-
-import com.example.txuser2.entity.UserB;
-
-public interface UserBService  {
-
-    String add(UserB userB);
-}
